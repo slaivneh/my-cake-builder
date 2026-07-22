f6dc61385ca14a8ed108e94369553bdcea6aa0a3
Minh Thu <duongthu7122006@gmail.com>
2026-07-22 22:21:15 +0700

    Sync AppRoutes from main to fix custom cake route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -15,46 +15,61 @@ import CakeDetail from "../pages/customer/CakeDetail";
 import Cart from "../pages/customer/Cart";
 import Checkout from "../pages/customer/Checkout";
 import OrderHistory from "../pages/customer/OrderHistory";
+import CustomerOrderDetail from "../pages/customer/CustomerOrderDetail";
 import OrderDetail from "../pages/customer/OrderDetail";
+import OrderSuccess from "../pages/customer/OrderSuccess";
 
-import CustomCake from "../pages/customCake/CustomCake";
+import CakeDesigner from "../pages/customCake/CakeDesigner";
 
 import OrderManagement from "../pages/admin/OrderManagement";
 
 import NotFound from "../pages/NotFound";
 
-
 function AppRoutes() {
-
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/home" replace />} />
+
       {/* Auth */}
+
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
       </Route>
 
-      {/* Customer */}
+      {/* Customer public */}
+
       <Route element={<CustomerLayout />}>
-        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+
+        <Route path="/cakes" element={<CakeList />} />
+
         <Route path="/cakes/:id" element={<CakeDetail />} />
 
+        {/* Customer protected */}
+
         <Route element={<ProtectedRoute role="customer" />}>
-          <Route path="/custom-cake" element={<CustomCake />} />
+          <Route path="/custom-cake" element={<CakeDesigner />} />
           <Route path="/cart" element={<Cart />} />
+
           <Route path="/checkout" element={<Checkout />} />
+
+          <Route path="/order-success/:id" element={<OrderSuccess />} />
+
           <Route path="/orders" element={<OrderHistory />} />
-          <Route path="/orders/:id" element={<OrderDetail />} />
+          <Route path="/orders/:id" element={<CustomerOrderDetail />} />
         </Route>
       </Route>
 
-      {/* Admin */}
-      <Route element={<ProtectedRoute role={["staff", "owner"]} />}>
+      {/* Owner */}
+
+      <Route element={<ProtectedRoute role="owner" />}>
         <Route element={<AdminLayout />}>
           <Route path="/admin/orders" element={<OrderManagement />} />
         </Route>
       </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
